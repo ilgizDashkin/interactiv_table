bc95aae5a113a16a5271958e72c86891088a7c04
ilgizDashkin <79374811484i@gmail.com>
2020-03-17 20:13:06 +0500

    end lesson

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React, { Component } from 'react';
 import Loader from './Loader/Loader';
 import Table from './Table/Table';
 import _ from 'lodash';
+import DetailRowView from './DetailRowView/DetailRowView';
 // https://abcinblog.blogspot.com/2019/02/react-i.html сделано по урокам
 
 class App extends Component {
-  state ={
+  state = {
     isLoading: true,
     data: [],
     sort: 'asc',  // 'desc'
     sortField: 'id', // поле по умолчанию
+    row: null,
   }
 
   async componentDidMount() {
     const response = await fetch(` http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`)
     const data = await response.json()
     //  console.log(data)
-    this.setState({ isLoading: false, data})
+    this.setState({
+      isLoading: false,
+      data: _.orderBy(data, this.state.sortField, this.state.sort)
+    })
   }
 
-  onSort = sortField => {  
+  onSort = sortField => {
     const cloneData = this.state.data.concat();
     const sortType = this.state.sort === 'asc' ? 'desc' : 'asc';
     const orderedData = _.orderBy(cloneData, sortField, sortType);
@@ -30,16 +35,25 @@ class App extends Component {
       sortField
     })
   }
-    
-  
+
+  onRowSelect = row => (
+    // console.log(row)
+    this.setState({row})
+  )
 
   render() {
     return (
       <div className="container">
-         {this.state.isLoading ? <Loader />: <Table 
-         data={this.state.data}
-         onSort={this.onSort}
-         />}
+        {this.state.isLoading ? <Loader /> : <Table
+          data={this.state.data}
+          onSort={this.onSort}
+          sort={this.state.sort}
+          sortField={this.state.sortField}
+          onRowSelect={this.onRowSelect}
+        />}
+        {
+        this.state.row ? <DetailRowView person={this.state.row} /> : null
+      }
       </div>
     );
   }
--- src/Table/Table.js
+++ src/Table/Table.js
@@ -4,16 +4,26 @@ export default props => (
     <table className="table">
         <thead>
             <tr>
-                <th onClick={props.onSort.bind(null, 'id')}>ID</th>
-                <th onClick={props.onSort.bind(null, 'firstName')}>First Name</th>
-                <th onClick={props.onSort.bind(null, 'lastName')}>Last Name</th>
-                <th onClick={props.onSort.bind(null, 'email')}>E-mail</th>
-                <th onClick={props.onSort.bind(null, 'phone')}>Phone</th>
+                <th onClick={props.onSort.bind(null, 'id')}>
+                    ID {props.sortField === 'id' ? <small>{props.sort}</small> : null}
+                </th>
+                <th onClick={props.onSort.bind(null, 'firstName')}>
+                    First Name {props.sortField === 'firstName' ? <small>{props.sort}</small> : null}
+                </th>
+                <th onClick={props.onSort.bind(null, 'lastName')}>
+                    Last Name {props.sortField === 'lastName' ? <small>{props.sort}</small> : null}
+                </th>
+                <th onClick={props.onSort.bind(null, 'email')}>
+                    E-mail {props.sortField === 'email' ? <small>{props.sort}</small> : null}
+                </th>
+                <th onClick={props.onSort.bind(null, 'phone')}>
+                    Phone {props.sortField === 'phone' ? <small>{props.sort}</small> : null}
+                </th>
             </tr>
         </thead>
         <tbody>
             {props.data.map(item => (
-                <tr key={item.id + item.phone}>
+                <tr key={item.id + item.phone } onClick={props.onRowSelect.bind(null, item)}>
                     <td>{item.id}</td>
                     <td>{item.firstName}</td>
                     <td>{item.lastName}</td>
